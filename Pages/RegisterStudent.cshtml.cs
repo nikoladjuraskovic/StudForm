94fa0356e9be7908e98a6c0f6e9255ccd1cbfe49
Nikola Djuraskovic <nikoladjuraskovic96@gmail.com>
2022-06-29 18:03:13 +0200

    commit asp

---
+++ Pages/RegisterStudent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPvezbeForma.Pages
+{
+    public class RegisterStudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
